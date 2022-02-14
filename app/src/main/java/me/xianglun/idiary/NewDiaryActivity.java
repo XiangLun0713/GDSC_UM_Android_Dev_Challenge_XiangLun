69e484b4ea47e6d4d3b610756f04780dbe685524
CHOO XIANG LUN <xlchoo25@gmail.com>
2022-02-14 16:50:52 +0800

    Add image by taking photo from camera feature added

--- app/src/main/java/me/xianglun/idiary/NewDiaryActivity.java
+++ app/src/main/java/me/xianglun/idiary/NewDiaryActivity.java
@@ -2,11 +2,13 @@ package me.xianglun.idiary;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +20,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,29 +28,43 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Random;
 
 public class NewDiaryActivity extends AppCompatActivity {
 
-    private static final int STORAGE_REQUEST_CODE = 100;
+    private static final int READ_STORAGE_REQUEST_CODE = 100;
     private static final int GALLERY_PICK_IMAGE_CODE = 200;
-    private String[] storagePermission;
-    private LinearLayout linearLayout;
+    private static final int CAMERA_REQUEST_CODE = 300;
+    private static final int CAMERA_TAKE_IMAGE_CODE = 400;
     private RelativeLayout imageTemplateLayout;
+    private String[] readStoragePermission;
+    private String[] cameraPermission;
+    private final String[] hints = {"How was your day?", "What's in your mind?",
+            "What was the best part of your day?", "How are you feeling today?",
+            "Did you learn anything new today?", "What are you most proud of today?",
+            "Did you get the chance to help anyone today?"};
+    private String currentPhotoPath;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_diary);
 
-        storagePermission = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+        readStoragePermission = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+        cameraPermission = new String[]{Manifest.permission.CAMERA};
         linearLayout = findViewById(R.id.new_diary_linear_layout);
         imageTemplateLayout = findViewById(R.id.diary_image_template);
 
@@ -61,6 +78,8 @@ public class NewDiaryActivity extends AppCompatActivity {
         String formattedDate = df.format(c);
         TextView dateAndTimeLabel = findViewById(R.id.date_and_time_label);
         dateAndTimeLabel.setText(formattedDate);
+        EditText diaryEditText = findViewById(R.id.diary_text);
+        diaryEditText.setHint(hints[new Random().nextInt(hints.length)]);
 
         EditText titleText = findViewById(R.id.diary_title);
         titleText.requestFocus();
@@ -100,22 +119,30 @@ public class NewDiaryActivity extends AppCompatActivity {
         dialog.setItems(items, (dialog1, which) -> {
             switch (which) {
                 case 0:
-                    if (!checkStoragePermission()) {
-                        requestStoragePermission();
+                    if (!checkReadStoragePermission()) {
+                        requestReadStoragePermission();
                     } else {
                         pickImageFromGallery();
                     }
                     break;
                 case 1:
-                    // TODO: 2/13/2022 insert code for camera
+                    if (!checkCameraPermission()) {
+                        requestCameraPermission();
+                    } else {
+                        dispatchTakePictureIntent();
+                    }
                     break;
             }
         });
         dialog.show();
     }
 
-    private void requestStoragePermission() {
-        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, cameraPermission, CAMERA_REQUEST_CODE);
+    }
+
+    private void requestReadStoragePermission() {
+        ActivityCompat.requestPermissions(this, readStoragePermission, READ_STORAGE_REQUEST_CODE);
     }
 
     private void pickImageFromGallery() {
@@ -124,16 +151,25 @@ public class NewDiaryActivity extends AppCompatActivity {
         startActivityForResult(intent, GALLERY_PICK_IMAGE_CODE);
     }
 
-    private boolean checkStoragePermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    private boolean checkCameraPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean checkReadStoragePermission() {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == STORAGE_REQUEST_CODE) {
+        System.out.println(Arrays.toString(grantResults));
+        if (requestCode == READ_STORAGE_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 pickImageFromGallery();
+            }
+        } else if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
             }
         }
     }
@@ -143,44 +179,94 @@ public class NewDiaryActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GALLERY_PICK_IMAGE_CODE && resultCode == RESULT_OK) {
             if (data != null) {
-                LinearLayout.LayoutParams linearParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                linearParams.setMargins(convertDpToPx(getApplicationContext(), 10), convertDpToPx(getApplicationContext(), 5), convertDpToPx(getApplicationContext(), 10), convertDpToPx(getApplicationContext(), 5));
-
-                View template = getLayoutInflater().inflate(R.layout.template_diary_image, linearLayout, false);
-                ImageView image = template.findViewById(R.id.diary_image);
-                image.setImageURI(data.getData());
-                FloatingActionButton deleteBtn = template.findViewById(R.id.delete_button);
-                deleteBtn.setOnClickListener(v -> {
-                    ViewParent parent = deleteBtn.getParent();
-                    int parentIndex = linearLayout.indexOfChild((View) parent);
-                    View editTextAfterImage = linearLayout.getChildAt(parentIndex + 1);
-                    View editTextBeforeImage = linearLayout.getChildAt(parentIndex - 1);
-                    if (editTextAfterImage instanceof EditText && editTextBeforeImage instanceof EditText) {
-                        if (((EditText) editTextBeforeImage).getText().toString().isEmpty() && !((EditText) editTextAfterImage).getText().toString().isEmpty()){
-                            ((EditText) editTextBeforeImage).setText(((EditText) editTextAfterImage).getText().toString());
-                        } else if (!((EditText) editTextBeforeImage).getText().toString().isEmpty() && !((EditText) editTextAfterImage).getText().toString().isEmpty()) {
-                            ((EditText) editTextBeforeImage).setText(((EditText) editTextBeforeImage).getText().toString().concat("\n").concat(((EditText) editTextAfterImage).getText().toString()));
-                        }
-                        linearLayout.removeView(editTextAfterImage);
-                        editTextBeforeImage.requestFocus();
-                        ((EditText) editTextBeforeImage).setSelection(((EditText) editTextBeforeImage).getText().length());
-                    }
-                    if (parent != null) {
-                        linearLayout.removeView((View) parent);
-                    }
-                });
-                linearLayout.addView(template, linearParams);
-
-                //add a new edit text view after the image
-                EditText newEditText = new EditText(this);
-                newEditText.setHint("Tell us more!");
-                newEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
-                newEditText.setBackgroundColor(Color.TRANSPARENT);
-                newEditText.setLineSpacing(convertDpToPx(this, 8), 1);
-                newEditText.requestFocus();
-                linearLayout.addView(newEditText, linearParams);
+                addDiaryImageTemplate(data.getData());
+            }
+        } else if (requestCode == CAMERA_TAKE_IMAGE_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                File file = new File(currentPhotoPath);
+                addDiaryImageTemplate(Uri.fromFile(file));
             }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this, "Error in creating file", Toast.LENGTH_SHORT).show();
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                System.out.println("image created");
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "me.xianglun.idiary.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_TAKE_IMAGE_CODE);
+            }
+        }
+    }
+
+    private void addDiaryImageTemplate(Uri data) {
+        LinearLayout.LayoutParams linearParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearParams.setMargins(convertDpToPx(getApplicationContext(), 10), convertDpToPx(getApplicationContext(), 5), convertDpToPx(getApplicationContext(), 10), convertDpToPx(getApplicationContext(), 5));
+
+        View template = getLayoutInflater().inflate(R.layout.template_diary_image, linearLayout, false);
+        ImageView image = template.findViewById(R.id.diary_image);
+        image.setImageURI(data);
+        FloatingActionButton deleteBtn = template.findViewById(R.id.delete_button);
+        deleteBtn.setOnClickListener(v -> {
+            ViewParent parent = deleteBtn.getParent();
+            int parentIndex = linearLayout.indexOfChild((View) parent);
+            View editTextAfterImage = linearLayout.getChildAt(parentIndex + 1);
+            View editTextBeforeImage = linearLayout.getChildAt(parentIndex - 1);
+            if (editTextAfterImage instanceof EditText && editTextBeforeImage instanceof EditText) {
+                if (((EditText) editTextBeforeImage).getText().toString().isEmpty() && !((EditText) editTextAfterImage).getText().toString().isEmpty()) {
+                    ((EditText) editTextBeforeImage).setText(((EditText) editTextAfterImage).getText().toString());
+                } else if (!((EditText) editTextBeforeImage).getText().toString().isEmpty() && !((EditText) editTextAfterImage).getText().toString().isEmpty()) {
+                    ((EditText) editTextBeforeImage).setText(((EditText) editTextBeforeImage).getText().toString().concat("\n").concat(((EditText) editTextAfterImage).getText().toString()));
+                }
+                linearLayout.removeView(editTextAfterImage);
+                editTextBeforeImage.requestFocus();
+                ((EditText) editTextBeforeImage).setSelection(((EditText) editTextBeforeImage).getText().length());
+            }
+            if (parent != null) {
+                linearLayout.removeView((View) parent);
+            }
+        });
+        linearLayout.addView(template, linearParams);
+
+        //add a new edit text view after the image
+        EditText newEditText = new EditText(this);
+        newEditText.setHint("Tell us more!");
+        newEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        newEditText.setBackgroundColor(Color.TRANSPARENT);
+        newEditText.setLineSpacing(convertDpToPx(this, 8), 1);
+        newEditText.requestFocus();
+        linearLayout.addView(newEditText, linearParams);
     }
 
     public int convertDpToPx(Context context, float dp) {
